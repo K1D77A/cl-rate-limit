f6244c7afca1c8bff56e96909f0c787e0197cf4e
K1D77A <k1llfrenzy@Hotmail.co.uk>
2020-06-13 21:03:22 +0100

    forgot to export the functions needed

--- src/package.lisp
+++ src/package.lisp
@@ -1,4 +1,13 @@
 ;;;; package.lisp
 
 (defpackage #:cl-rate-limit
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:make-bucket
+           #:shutdown-bucket
+           #:purge-bucket
+           #:stop-unlock
+           #:emergency-stop-unlock-thread
+           #:adjust-rate-per-second
+           #:bucket-execute
+           #:execute-func-when-limiter-free
+           ))
